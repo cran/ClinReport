daeedd86cda9084ec3fbef05a0be0a757e09c956
Jean-Francois COLLIN <jfcollin@live.fr>
2019-02-27 10:20:17 +0000

    version 0.9.1.8

--- inst/dev_check_build.R
+++ inst/dev_check_build.R
@@ -7,22 +7,47 @@
 #Sys.setenv(PATH = paste(Sys.getenv("PATH"),
 # "C:/Users/jfcollin/AppData/Local/Programs/MiKTeX 2.9/miktex/bin/x64",
 # sep=.Platform$path.sep))
+#
+#
+Sys.setenv(PATH = paste(Sys.getenv("PATH"),
+				"C:/Users/jfcollin/AppData/Local/Pandoc",
+				sep=.Platform$path.sep))
+#
 
 
 library(devtools)
 library(roxygen2)
 library(usethis)
+library(rmarkdown)
 
-setwd("C:\\Users\\jfcollin\\Google Drive\\Dev\\ClinReport")
+setwd("C:\\Users\\jfcollin\\git")
 
-roxygenize("C:\\Users\\jfcollin\\Google Drive\\Dev\\ClinReport")
+roxygenize("C:\\Users\\jfcollin\\git")
 
 check(args ="--as-cran")
 
-#usethis::use_readme_rmd()
-#usethis::use_news_md()
 
 install()
 build()
+
+
+
+
+###############################################
+#  readme
+###############################################
+
+render("C:\\Users\\jfcollin\\git\\ClinReport\\inst\\README.Rmd")
+#shell.exec("C:\\Users\\jfcollin\\git\\ClinReport\\README.html")
+
+
+
+
+
+
+
+
+
+
 
 
